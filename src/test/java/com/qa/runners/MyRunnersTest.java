942fe64519368621457374090c50279c15c906e6
vishnu93tr <vishnu26121993@gmail.com>
2021-03-26 12:12:06 +0530

    cucumber with junit

---
+++ src/test/java/com/qa/runners/MyRunnersTest.java
@@ -0,0 +1,46 @@
+package com.qa.runners;
+
+import com.qa.utils.DriverManager;
+import com.qa.utils.GlobalParams;
+import com.qa.utils.ServerManager;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.apache.logging.log4j.ThreadContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty",
+        "html:target/cucumber",
+        "summary",
+        "de.monochromata.cucumber.report.PrettyReports:target/cucumber-html-reports"},
+        features = {"src/test/resources"},
+        glue = {"com.qa.stepdef"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        monochrome=true,
+        tags = "@test")
+public class MyRunnersTest {
+    @BeforeClass
+    public static void init() throws Exception {
+        GlobalParams params=new GlobalParams();
+        params.initializeGlobalParams();
+        ThreadContext.put("ROUTINGKEY",params.getPlatformName()+"_"+params.getDeviceName());
+        new ServerManager().startServer();
+        new DriverManager().initializeDriver();
+    }
+    @AfterClass
+    public static void quit(){
+        DriverManager driverManager=new DriverManager();
+        if(driverManager.getDriver()!=null){
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
+        }
+        ServerManager serverManager=new ServerManager();
+        if(serverManager.getServer()!=null){
+            serverManager.getServer().stop();
+        }
+    }
+}
